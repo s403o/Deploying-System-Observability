c3346354a207211388fd323fb5485f36b7ce75f0
s403o <eslam.adel.me@gmail.com>
2022-12-08 02:21:25 +0200

    add s3 bucket for terraform

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-terraform-course1"
+     bucket = "sre1-terraform1"
      key    = "terraform/terraform.tfstate"
      region = "us-east-2"
    }
